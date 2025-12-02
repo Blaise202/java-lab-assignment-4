38ae5860339f8d3777a0181ca8821e384bb8c330
Blaise202 <iblaise2023@gmail.com>
2025-12-02 10:37:53 +0000

    Create MedicalRecord.java

---
+++ MedicalRecord.java
@@ -0,0 +1,16 @@
+public class MedicalRecord {
+  public String date;
+  public String description;
+
+  public MedicalRecord(String date, String description){
+    this.date = date;
+    this.description = date;
+  }
+
+  public String getDate(){
+    return date;
+  }
+  public String getDescription(){
+    return description;
+  }
+}
