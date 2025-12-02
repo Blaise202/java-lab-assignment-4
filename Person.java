601d58beaedbcc6149fccb005875251990e916aa
Blaise202 <iblaise2023@gmail.com>
2025-12-02 10:32:15 +0000

    updated Person.java

--- Person.java
+++ Person.java
@@ -1,3 +1,26 @@
-public class Person {
-  
+public abstract class Person {
+  private String email;
+  private String name;
+  private String phone;
+
+  public Person(String email, String name, String phone){
+    this.email = email;
+    this.name = name;
+    this.phone = phone;
+  }
+
+  public String getEmail(){
+    return email;
+  }
+  public String getName(){
+    return name;
+  }
+  public String getPhone(){
+    return phone;
+  }
+
+  @Override
+  public String toString(){
+    return name + " ("+ email +")";
+  }
 }
