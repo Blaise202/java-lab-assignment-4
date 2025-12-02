3da484db7e4ee82d9792d2ae4f3415a0239a13eb
Blaise202 <iblaise2023@gmail.com>
2025-12-02 09:48:50 +0000

    FileOperations interface

---
+++ interfaces/FileOperations.java
@@ -0,0 +1,8 @@
+package interfaces;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public interface FileOperations{
+  void saveToFile(String filename) throws IOException;
+  void loadFromFile(String filename) throws IOException, FileNotFoundException;
+}
